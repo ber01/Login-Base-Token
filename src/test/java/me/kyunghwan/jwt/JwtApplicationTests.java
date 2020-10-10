a790deb9af3789b8b8a118cbe9ed9840272b6588
ber01 <ber01@naver.com>
2020-10-10 19:34:12 +0900

    Initial commit

---
+++ src/test/java/me/kyunghwan/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kyunghwan.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+        JwtApplication.main(new String[]{});
+    }
+
+}
