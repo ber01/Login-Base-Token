616cfe5adb21c698f7f78500e5874cb551189d67
ber01 <ber01@naver.com>
2020-10-10 23:11:17 +0900

    Profile 분리
    - 테스트(h2)
    - 메인(MySQL)

--- src/test/java/me/kyunghwan/jwt/JwtApplicationTests.java
+++ src/test/java/me/kyunghwan/jwt/JwtApplicationTests.java
@@ -2,7 +2,10 @@ package me.kyunghwan.jwt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("dev")
 class JwtApplicationTests {
 
     @Test
