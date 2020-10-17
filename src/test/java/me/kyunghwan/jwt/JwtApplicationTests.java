4da06d48c681987ade62bff7086dd012b1dd0138
ber01 <ber01@naver.com>
2020-10-18 04:42:26 +0900

    Updated test

--- src/test/java/me/kyunghwan/jwt/JwtApplicationTests.java
+++ src/test/java/me/kyunghwan/jwt/JwtApplicationTests.java
@@ -1,16 +1,12 @@
 package me.kyunghwan.jwt;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("dev")
 class JwtApplicationTests {
 
     @Test
     void contextLoads() {
-        JwtApplication.main(new String[]{});
+
     }
 
 }
